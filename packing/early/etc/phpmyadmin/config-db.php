5592eee84793420b21e38182657d242df72b52cf
Mark Ratjens <mark@ratjens.com>
2020-11-24 14:39:23 +1100

    fix broken interpolations

---
+++ packing/early/etc/phpmyadmin/config-db.php
@@ -0,0 +1,10 @@
+<?php
+##
+
+$dbuser='^^mariadb.username^^';
+$dbpass='^^mariadb.password^^';
+$basepath='';
+$dbname='^^mariadb.databasename^^';
+$dbserver='^^mariadb.hostname^^';
+$dbport='3306';
+$dbtype='mariadb';
