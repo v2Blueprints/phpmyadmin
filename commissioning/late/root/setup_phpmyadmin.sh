2ae59c811638242dc62902941bfd7ed80e054c38
James Vodanovich <james@jvodan.com>
2021-12-14 10:07:57 +1100

    write startup complete flag

--- commissioning/late/root/setup_phpmyadmin.sh
+++ commissioning/late/root/setup_phpmyadmin.sh
@@ -2,3 +2,4 @@
 
 mysql -h ^^mariadb.hostname^^ --user ^^mariadb.username^^ --password=^^mariadb.password^^ ^^mariadb.database_name^^ < /usr/share/phpMyAdmin/sql/create_tables.sql
 
+touch /var/run/status/startup_complete
